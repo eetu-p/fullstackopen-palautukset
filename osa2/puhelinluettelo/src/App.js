1f311d7fafeabd73eb28b84c5c4b9d9fb43195c6
Eetu <ejp>
2022-10-25 16:35:37 +0300

    Tehty 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import AddPerson from './AddPerson';
 import List from './List';
 import Search from './Search';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "0441234567" }
-  ]) 
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = event => {
     event.preventDefault();
