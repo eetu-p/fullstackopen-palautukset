1552fa058c73849f2bbc22b0a753518c0afac67c
Eetu <ejp>
2022-11-08 20:26:55 +0200

    Tehty 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,25 @@ const totalLikes = blogs => {
   return blogs.reduce((previousValue, currentValue) => { return previousValue + currentValue.likes }, 0)
 }
 
+const favoriteBlog = blogs => {
+  return blogs.reduce((previousValue, currentValue) => {
+    if (currentValue.likes > previousValue.likes) {
+      return {
+        title: currentValue.title,
+        author: currentValue.author,
+        likes: currentValue.likes
+      }
+    }
+    return {
+      title: previousValue.title,
+      author: previousValue.author,
+      likes: previousValue.likes
+    }
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
