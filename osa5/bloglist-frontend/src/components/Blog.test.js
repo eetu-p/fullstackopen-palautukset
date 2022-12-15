c7e088b770a7942a126648b5d75f02d22903213b
Eetu <ejp>
2022-12-15 14:44:39 +0200

    Tehty 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test("only blog title is rendered by default", () => {
 
@@ -36,5 +37,43 @@ test("only blog title is rendered by default", () => {
   expect(authorElement).toBeNull()
   expect(urlElement).toBeNull()
   expect(likesElement).toBeNull()
+})
 
+test("all blog info is shown when the show button is pressed", async () => {
+
+  const blog = {
+    title: "Blog title",
+    author: "Blog author",
+    url: "Blog URL",
+    likes: 1,
+    user: {
+      username: "Test user",
+      name: "Matti Meikäläinen",
+      id: "1234"
+    },
+    id: "ABCD"
+  }
+
+  const userInfo = 	{
+		username: "Test user",
+		name: "Matti Meikäläinen",
+		blogs: [],
+		id: "1234"
+	}
+
+  render(<Blog blog={blog} user={userInfo} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText("Show")
+  await user.click(button)
+
+  const titleElement = screen.getByText("Blog title")
+  const authorElement = screen.getByText("Blog author")
+  const urlElement = screen.getByText("Blog URL")
+  const likesElement = screen.getByText("1 like(s)")
+
+  expect(titleElement).toBeDefined()
+  expect(authorElement).toBeDefined()
+  expect(urlElement).toBeDefined()
+  expect(likesElement).toBeDefined()
 })
